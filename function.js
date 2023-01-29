f21b9265fc6e3fb9f63c133e064724bd0822658e
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-29 22:10:20 +0600

    function start of my life

---
+++ function.js
@@ -0,0 +1,10 @@
+function startFunction(){
+    console.log('Start The fan')
+    console.log('Watvhing Js tutorial')
+    console.log('Press The swich')
+}
+
+startFunction();
+console.log('Others Function')
+startFunction()
+console.log('Others tutorial')
