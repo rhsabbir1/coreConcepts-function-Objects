d0a145608b57d385f5d6df71fc0833ca588a27d0
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-29 22:44:12 +0600

    adding name

---
+++ function-paramiter.js
@@ -0,0 +1,36 @@
+function buyBook(money){
+    console.log(money)
+    console.log('This is book Shop')
+}
+// buyBook(100)
+
+function bringSinghara(money){
+    console.log("Total tk :",money)
+}
+
+var taka = 200;
+// bringSinghara(taka)
+
+
+function add(num1 , num2){
+    console.log(num1 , num2)
+}
+
+// add(12, 14)
+
+function sum(a, b, c, d, e){
+    var sum = a+ b+ c+ d+ e;
+    console.log(a ,b, c ,d ,e ,"=",sum)
+}
+
+// sum(12 , 20 , 21, 2 , 5 )
+
+
+function addFullName (firstName , lastName){
+    console.log(firstName+" "+lastName)
+}
+
+var firstName = 'Sabbir';
+var lastName = 'Rahman';
+
+addFullName(firstName,lastName)
