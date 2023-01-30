8ab4827ca66a2e852230e53d32836f80e3feb887
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-30 15:15:49 +0600

    object Declear

---
+++ objects.js
@@ -0,0 +1,27 @@
+// Objects Declear 
+
+var person ={
+    name : "sabbir",
+    Roll : 15,
+    class : "8th"
+}
+
+var phone= {
+    name : 'Sumsang',
+    colour :" Hash",
+    stroge : "64gb"
+}
+
+var myComputer = {
+    name :"Sp",
+    colour : "Black",
+    prosacor : 'Core i3',
+    price : 45000
+}
+
+var computerPrice = myComputer.price;
+console.log(myComputer)
+console.log(computerPrice)
+
+myComputer.price = 50000;
+console.log(myComputer)
